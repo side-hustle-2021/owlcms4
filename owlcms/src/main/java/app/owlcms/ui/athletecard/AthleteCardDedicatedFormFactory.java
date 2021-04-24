204f09debc8928a931c4f158f15f0b197760fb35
Afsan Abdulali Gujarati <afsan.gujarati@gmail.com>
2021-04-24 17:32:32 -0600

    removed unused import

--- owlcms/src/main/java/app/owlcms/ui/athletecard/AthleteCardDedicatedFormFactory.java
+++ owlcms/src/main/java/app/owlcms/ui/athletecard/AthleteCardDedicatedFormFactory.java
@@ -19,7 +19,6 @@ import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.converter.StringToDoubleConverter;
-import com.vaadin.flow.data.value.ValueChangeMode;
 
 import app.owlcms.components.fields.ValidationUtils;
 import app.owlcms.data.athlete.Athlete;
